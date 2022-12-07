c99d6f5d53377d6d7e03c391ef9963eaf8403f56
krisine <kristine.nyaga@student.moringaschool.com>
2022-12-07 07:37:19 +0300

    done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,16 +7,13 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    # iinitializes viewpage counter to zero
     session[:page_views] ||= 0
     session[:page_views] = session[:page_views].to_i + 1
     
-    # checks if the view page is more or less than 3
     if session[:page_views] <= 3
       article = Article.find(params[:id])
       render json: article
     else 
-      # return error message if condition is not met
       render json: {error: "Maximum pageview limit reached"}, status: :unauthorized
     end
   end
